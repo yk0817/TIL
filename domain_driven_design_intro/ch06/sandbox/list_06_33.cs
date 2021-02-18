06c2362f5d1c66778f031ef70461aa1bd039e26c
yamaoto_ken <ken.yamamoto@excite.jp>
2021-02-19 08:47:52 +0900

    写経

---
+++ domain_driven_design_intro/ch06/sandbox/list_06_33.cs
@@ -0,0 +1,27 @@
+public class UserRegisterService
+{
+  private readonly IUserRepository userRepository;
+  private readonly UserService userService;
+
+  public UserRegisterService(IUserRepository userRepository, UserService userService)
+  {
+    this.userRepository = userRepository;
+    this.userService = userService;
+  }
+
+  public void Handle(UserRegisterCommand command)
+  {
+    var userName = new UserName(command.Name);
+
+    var user = new User(
+      userName
+    );
+
+    if (userService.Exists(user))
+    {
+      throw new CanNotRegisterUserException(user, "ユーザーは既に存在しています");
+    }
+
+    userRepository.Save(user);
+  }
+}
